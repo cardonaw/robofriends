8ea1bde12ae1093690b30a670a37c1974ad65836
William A. Cardona <williamxcardona@gmail.com>
2024-04-14 09:41:51 -0400

    update app.js like it supose to be

--- src/containers/App.js
+++ src/containers/App.js
@@ -38,7 +38,7 @@ class App extends Component {
             <h1 className="tc">Loading...</h1> :
             (
                 <div className="tc">
-                    <h1 className="f2">RoboFriends baby</h1>
+                    <h1 className="f2">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
                         <ErrorBoundry>
